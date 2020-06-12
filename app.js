8c08f76c0a477f383cd1db9cc000e5d6f1201eac
oleoot <oleoot28@gmail.com>
2020-06-12 16:57:03 +0300

    Basic express server + 2 routes created

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+
+const app = express()
+
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: "Welcome to the API"
+    })
+})
+
+app.post('/api/posts', (req, res) => {
+    res.json({
+        message: "Post created"
+    })
+})
+app.listen(5000, () => console.log("Server startet at 5000"))
