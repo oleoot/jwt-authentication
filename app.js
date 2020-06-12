73016fbe124cde7b788e4bcd8849f978c1d344c2
oleoot <oleoot28@gmail.com>
2020-06-12 17:01:18 +0300

    adding jwt post request

--- app.js
+++ app.js
@@ -16,4 +16,22 @@ app.post('/api/posts', (req, res) => {
         message: "Post created"
     })
 })
+
+app.post('/api/login', (req, res) => {
+    // User
+    const user = {
+        id: 1,
+        username: 'oleg',
+        email: 'oleg@gmail.com'
+    }
+    jwt.sign({
+        user: user
+    }, 'secretkey', (error, token) => {
+        res.json({
+            token
+        })
+    })
+})
+
+
 app.listen(5000, () => console.log("Server startet at 5000"))
