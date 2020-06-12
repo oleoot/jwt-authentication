4f044b6a46e29273c6ea9ee722a8bccb61693341
oleoot <oleoot28@gmail.com>
2020-06-12 17:08:26 +0300

    Formatting and sending token via function

--- app.js
+++ app.js
@@ -42,7 +42,14 @@ function verifyToken(req, res, next) {
     const bearerHeader = req.headers['authorization'];
     // Check if bearer is undefined
     if (typeof bearerHeader == !undefined) {
-
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from arr
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken
+        // Call next
+        next()
     } else {
         //Forbidden
         res.sendStatus(403)
